c291aacb432bb8253d55161035aeb319f24a3e8f
Damar2121 <damar.vivoy51@gmail.com>
2025-10-30 16:10:20 +0700

    Membuat HorizontalDivider dengan modifier padding bottom padding thickness memanggil divider tipis dan warnanya dark grey

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -110,6 +110,18 @@ fun FormDataDiri(modifier: Modifier
 
         }
 
+        HorizontalDivider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(R.dimen.padding_medium),
+                top = dimensionResource(
+                    id = R.dimen.padding_medium
+                )),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
+
+
+
 
 
 
