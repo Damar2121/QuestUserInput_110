03e5a8f6462c9e9f8ee20ce7c24190cdb0525bb6
Damar2121 <damar.vivoy51@gmail.com>
2025-10-30 15:23:11 +0700

    Membuat column dengan padding 50.dp

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -1,9 +1,15 @@
 package com.example.praktikum4
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun FormDataDiri(modifier: Modifier
@@ -17,4 +23,12 @@ fun FormDataDiri(modifier: Modifier
     var jenis by remember { mutableStateOf("") }
 
     var gender:List<String> = listOf("Laki-laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+    }
+
+
 }
